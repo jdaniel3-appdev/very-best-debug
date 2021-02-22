9ecdb056e77f96cee383d7bae30aa2eb8d910ffd
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-02-22 18:15:43 +0000

    Commit 2 Fully refactored, rails grade 100%

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -16,31 +16,32 @@ class VenuesController < ApplicationController
   end
 
   def create
-    @venue = Venue.new
+    venue = Venue.new
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
+    venue.name = params.fetch("query_name")
+    venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
 
-    redirect_to("/venues/#{venue.name}")
+    redirect_to("/venues/#{venue.id}")
   end
   
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
-    venue.neighborhood = params.fetch("query_neighborhood")
-    venue.save
+    matching_venues = Venue.where({ :id => the_id })
+    the_venue = matching_venues.at(0)
+    the_venue.address = params.fetch("query_address")
+    the_venue.name = params.fetch("query_name")
+    the_venue.neighborhood = params.fetch("query_neighborhood")
+    the_venue.save
     
-    redirect_to("/venues/#{venue.id}")
+    redirect_to("/venues/#{the_venue.id}")
   end
 
   def destroy
     the_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
+    venue = matching_venues.at(0)
     venue.destroy
 
     redirect_to("/venues")
